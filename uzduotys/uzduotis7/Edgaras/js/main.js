0cba87e569a8e9cc34e2b185f6e0a8057feee1bd
Ubavicius <31346083+Ubavicius@users.noreply.github.com>
2017-09-12 19:10:29 +0300

    7 diena

---
+++ uzduotys/uzduotis7/Edgaras/js/main.js
@@ -0,0 +1,45 @@
+console.log("Labas");
+x= 5 % 2;
+console.log("x:", x);
+
+
+x=10;
+y=10;
+
+// tikrina ar x yra lygu 5
+if (x===10) {
+    // jeigu teisa
+    console.log("taip x lygu 10");
+}
+else {
+// jeigu netiesa
+    console.log("NE x nelygu 10");
+}
+// // IR/AND ---> privalo sutapto
+// if (username == xxx && password == yyy){
+// }
+// // ARBA/OR ---> neprivalo sutapti
+// if (username == xxx || password == yyy){
+// }
+
+// tikrina salyga ir kintamuju tipus
+// if (x === "10" );
+// if ( x != 18 );
+
+// skaiciuoja skaitmenis
+text = "Tadas ejo kopeciom";
+textIlgis = text.length;
+console.log("teksto ilgis:" + textIlgis);
+
+// pamato ir pakeicia keiksmazodzius
+str = "Please visit Microsoft!";
+console.log(str);
+var n = str.replace("Microsoft", "W3Schools");
+console.log(n);
+
+// galima sudalinti raidem, zodziai, sakiniais
+str = "nieko siandien nesuprantu";
+sudalintas = str.split(" ");
+console.log(sudalintas);
+
+//
