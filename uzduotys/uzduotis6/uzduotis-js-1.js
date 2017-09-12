8343280674e42ff24b3c197f95e81c214143bab2
tautvydascoding <tautvydascoding@gmail.com>
2017-09-12 13:43:45 +0300

    7 diena

--- uzduotys/uzduotis6/uzduotis-js-1.js
+++ uzduotys/uzduotis6/uzduotis-js-1.js
@@ -1,44 +1,25 @@
 console.log("labas");
- 
- 
+
+
 //// IDEA: uzduotis 1
 //// A) sukurti 3 kintamuosius: vardas, pavarde, klase
-//// B) priskiti jiems reiksmes
+//// B) priskirti jiems reiksmes
 //// C) isvesti juos i konsole
 
 //// IDEA: uzduotis 2
 //// sukurti f-ja "spausdinti()", ir joje sukurti 3 kintamuoius ir juos isvesti i konsole
- 
- 
- 
-//// IDEA: uzduotis 3
-// isvesti kintamuosius vienoje eilute 
 
- 
+
+
+//// IDEA: uzduotis 3
+// isvesti kintamuosius vienoje eilute
+
+
 // IDEA: uzduotis 4 - papildomos uzduotys   ------------
-// A) sukurti f-ja, kuri isveda i konsole teksta: "Labas vakasras Kaune!"
+// A) sukurti f-ja, kuri isveda i konsole teksta: "Labas vakaras Kaune!"
 // B) sukurti f-ja SpausdinuVarda(vardas), kuri isveda i konsole teksta:  paduota kintamaji
 // C) sukurti f-ja kelimasKvadratu(x), kuri isveda i konsole paduoto kintamo reiksme pakelta kvadratu ( pvz x*x)
-// D) sukurti f-ja skaiciavimai(x,y,z), kuri isveda i konsole x + 2, y + 4, z *2
 
- 
+// D sukurti f-ja kuri atspausdina paduotas reiksmes pazymiai(a,b,c)
 
- 
-//// ----------------IDEA: uzduotis 4 -------------------
-//// sukurti f-ja kuri atspausdina paduotas reiksmes
-//// pvz: spausdinti(4.5, 0, "geras lankomumas");
-//
- 
- 
- 
-
-
-
- 
-
-
-
-
-
-
-
+// E) sukurti f-ja skaiciavimai(x,y,z), kuri isveda i konsole x + 2, y + 4, z *2
---
+++ uzduotys/uzduotis7/tautvydas/0-basics/main.js
@@ -0,0 +1,67 @@
+console.log("Labas");
+
+
+x = 5 % 2; // 1 - isveda liekana  5:2 = 2 sveiki ir 1 liekana
+console.log("x:", x);
+
+
+x++;
+console.log("x++:", x);
+x--;
+console.log("x--:", x);
+--x;
+console.log("--x:", x);
+
+
+ x = 10;
+
+// tikrina salyga
+ if (x == "10"   ) {
+    // jeigu tiesa
+    console.log("taip x lygu 10");
+} else {
+    // jeigu netiesa
+    console.log("NE x nelygu 10");
+}
+
+//                   ir
+//  if (usename == xxx  &&  password == yyyy) {
+//  }
+// //                   arba
+//  if (usename == xxx  ||  password == yyyy) {
+//  }
+
+
+
+// tikrina salyga ir kintamuju tipus (tekstas ar skaiciai)
+ // if (x === "10")
+ // if ( x != 18 )
+
+
+
+ text = "Tadas ejo kopeciom";
+textIlgis = text.length;  // 18
+console.log("teksto ilgis: " + textIlgis);
+
+
+
+
+
+
+str = "Please visit rupus miltai!";
+console.log(str);
+var n = str.replace("rupus miltai", "r****");
+console.log(n);
+
+
+str = "Please visit rupus miltai!";
+sudalintas = str.split(" ");
+console.log(sudalintas);
+
+
+
+
+
+
+
+ //
