b673106f7cd01b3bf59bb998dde2b606051f9682
MAN\Mann <mantvydas.racickas@gmail.com>
2017-09-15 09:56:42 +0300

    js 9

--- uzduotys/uzduotis9/uzduotis-js-arrays5.js
+++ uzduotys/uzduotis9/uzduotis-js-arrays5.js
@@ -1,11 +1,11 @@
 // apsilimui
-// 1 kursime nauja masyva uzpildyta, atsitiktiniais skaiciais nuo 10 - 100  
-// 2uzdavinys: pakeisti atsitiktinius skaicius, kurie yra lyginai i "lyginis"
+// 1 kursime nauja masyva uzpildyta, atsitiktiniais skaiciais nuo 10 - 100
+// 2uzdavinys: pakeisti atsitiktinius skaicius, kurie yra lyginiai i "lyginis"
 
 
 // rimta
 // 1 kursime nauja masyva uzpildyta, atsitiktiniais skaiciais nuo 50 - 200 (pagamintomis datalemis per diena)
-// 1 burtu keliu 10 %  paversti minus reikememis ( padauginti is  -1)
+// 1 burtu keliu 10 % tikimybe paversti minus reikememis ( padauginti is  -1)
 // 1. uzdavinys: visus skaicius paversti teigiamas (is masyvo parts)
 // 2. surasti geriausia darbuotoja
 // 3. surasti blogiausia darbuotoja
