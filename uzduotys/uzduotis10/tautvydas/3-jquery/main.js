80fb7c43f97ed631a863231b92e20525a9ca11a3
Stokna <31610863+Stokna@users.noreply.github.com>
2017-09-18 08:53:36 +0300

    life

--- uzduotys/uzduotis10/tautvydas/3-jquery/main.js
+++ uzduotys/uzduotis10/tautvydas/3-jquery/main.js
@@ -12,3 +12,57 @@ var text2 = $('.container').html();
 var text3 = $('#wrapper').html();
 
 console.log(text1);
+
+$(".mygtukas").click(function(event){
+$("img:last").toggle(1000);
+});
+
+//cool function. leidzia panaudot funkcija vienodiem elementam, bet tik ant to, kuri 'paspaudi'
+// $("img").mouseenter(function(event){
+// $(this).toggle(1000);
+// });
+
+var stilius = {
+  "color": "red",
+  "background-color": "#000",
+  'font-size': "78px",
+  "margin": "200px 450px"
+};
+
+$('h1').css(stilius);
+
+// var width = {
+//   "width": "200px"
+// };
+// $("img").click(function(event){
+//     $(this).animate(width);
+//     });
+
+$('img').on("click", function(){
+  var animacijosVeiksmai = {
+    width: "200px",
+    height: "200px"
+  };
+  $(this).animate(animacijosVeiksmai);
+});
+
+
+
+// function veiksmaiPaspaudus() {
+//
+// }
+
+
+
+
+
+
+// variantas #1
+$(document).ready(function(){
+  //cia parasytas kodas veiks tik kai uzsikraus visa svetaine
+});
+//variantas #2
+$(function() {
+  //cia parasytas kodas veiks tik kai uzsikraus visa svetaine
+console.log("sveikinu uzkrovus svetaine");
+});
