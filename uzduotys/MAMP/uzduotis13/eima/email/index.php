9f02c39fb07099b3312f727a7897e0a037db75ff
eimulex <eimulex@gmail.com>
2017-09-20 13:37:06 +0300

    asd

---
+++ uzduotys/MAMP/uzduotis13/eima/email/index.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>
+
+        </title>
+    </head>
+    <body>
+
+        <form   action="siusti.php" method="post">
+            <label for="">El.Pastas</label>
+            <input type="email" name="elpastas" value="">
+            <br><br>
+            <label>Antraste</label>
+            <input type="text" name="antraste" value="" maxlength="150">
+            <br><br>
+            <label >Jusu klausimas</label>
+            <textarea name="zinute" rows="8" cols="80"></textarea>
+
+            <input type="submit" name="" value="Klausti">
+        </form>
+
+    </body>
+</html>
