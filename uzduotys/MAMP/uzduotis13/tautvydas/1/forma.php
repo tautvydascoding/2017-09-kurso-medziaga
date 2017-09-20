cf4fc89eb1dd6938f31b52b135ff01bd074a3189
tautvydascoding <tautvydascoding@gmail.com>
2017-09-20 11:28:15 +0300

    13 diena

--- uzduotys/MAMP/uzduotis13/tautvydas/1/forma.php
+++ uzduotys/MAMP/uzduotis13/tautvydas/1/forma.php
@@ -3,7 +3,27 @@
 echo "<h1>Uzsiregistravo :</h1>";
 
 
-// $vardas = $_POST[];
+$vardas = $_POST["name"];
+// pasalinam tarpus pries/uz
+$vardas = trim($vardas);
+$zinute = "";
+
+if ( strlen($vardas) <= 3) {
+    $zinute = "Ivestas vardas per trumpas!";
+} else {
+    $zinute = "Liuks";
+}
+$kodas1 = $_POST["password1"];
+$kodas2 = $_POST["password2"];
+
+
+if ($kodas1 === $kodas2) {
+    $zinute = "Sutapo pass";
+} else  {
+    $zinute = "Slaptazodziai nesutampa!!!";
+}
+
+echo $zinute;
 print_r($_POST);
 
 
