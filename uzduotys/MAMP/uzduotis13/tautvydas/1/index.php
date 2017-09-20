e9fb0e24002e4cc6539614dd15d5674d9843abca
tautvydascoding <tautvydascoding@gmail.com>
2017-09-20 10:33:25 +0300

    13 diena

---
+++ uzduotys/MAMP/uzduotis13/tautvydas/1/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title></title>
+    </head>
+    <body>
+
+
+        <h1>Post Get testavimas</h1>
+
+        <form class="" action="forma.php" method="get">
+            <input type="text" name="name" value="">
+            <input type="text" name="lname" value="">
+            <input type="number" name="age" value="">
+            <input type="password" name="password" value="">
+            <input type="email" name="pastas" value="" required>
+
+            <input type="submit" name="" value="Register">
+            <!-- ARBA -->
+            <button type="submit" name="button">Register</button>
+        </form>
+
+
+
+
+    </body>
+</html>
