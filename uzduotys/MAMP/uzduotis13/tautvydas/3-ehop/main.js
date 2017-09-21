ec50ed9fc0c78234177b6739a95e25cf67e612a3
tautvydascoding <tautvydascoding@gmail.com>
2017-09-21 09:36:46 +0300

    13 diena

--- uzduotys/MAMP/uzduotis13/tautvydas/3-ehop/main.js
+++ uzduotys/MAMP/uzduotis13/tautvydas/3-ehop/main.js
@@ -1,1 +1,28 @@
 console.log("labas");
+
+// $paveiksliukas = $('.w3-row.w3-grayscale  img');
+// console.log($paveiksliukas[1]);
+// $paveiksliukas.on("click", function(){
+ $('.w3-row.w3-grayscale  img').on("click", function(){
+
+    $imgKopija = this.cloneNode(true);
+    $imgTevas = this.parentNode;
+
+    $imgTevas.insertBefore($imgKopija, this);
+
+    // console.log("123");
+    $stilius = {
+        position: 'absolute',
+        width : "14%"
+    }
+    $($imgKopija).css($stilius);
+    $($imgKopija).animate({
+            opacity: 0,
+            top: "20px",
+            right: "20px",
+            width: 0
+    }, 3000);
+    // $(this).animate({
+    //         top: "20px"
+    // }, 3000);
+});
