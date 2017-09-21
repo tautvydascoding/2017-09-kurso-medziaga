5115edad9ca5cf3bf0d80cfde08e4865c3a9af77
tautvydascoding <tautvydascoding@gmail.com>
2017-09-21 15:34:06 +0300

    13 diena

--- uzduotys/MAMP/uzduotis14/tautvydas/3-php-extends-1/index.php
+++ uzduotys/MAMP/uzduotis14/tautvydas/3-php-extends-1/index.php
@@ -16,7 +16,7 @@ class Zuvis extends Animal {
     public function setMaistas ($y) {
         $this->maistas = $y;
         $this->gelavandenis = $this->y;
-        echo " Bandome issivesti protected reikeme: " . $this->maistas;
+        echo " Bandome issivesti protected reikeme: " . $this->maistas. "<br>";
     }
 }
 
@@ -41,11 +41,52 @@ $karosas->setMaistas("dumblas");
 // kintamieji: private username = 'Tim', privattus password = 'Dragon', public lastLoginTime
 // f-jos : public login($uname, $pass) , kuri patikrina ar vartotojo spaltazodis yra toks kaip vartotojo
 
-$vartotojas = new User();
-$arToksEgzistuoja = $vartotojas->login("Gregor", "Dragon");
-echo $arToksEgzistuoja;
+// $vartotojas = new User();
+// $arToksEgzistuoja = $vartotojas->login("Gregor", "Dragon");
+// echo $arToksEgzistuoja;
 
 
 // uzduotis 2 sukurti Admin kalse , kuri paveldi User klase
 // ir turi kintamuosius: $userRights
-// turi f-ja : grandUser() , kuri pakeicia vartotojo galias / teisias
+// turi f-ja : changeUsername($newName) , kuri pakeicia vartotojo varda
+class User {
+    private $username = "Tim";
+    private $password = "Dragon";
+    public $lastLoginTime = "2017-08-02";
+    public function checkLogin($user, $pass) {
+        if ( $user === $this->username &&  $pass === $this->password) {
+            echo "Taip, toks vartotojas egzistuoja. Sveikinu prisijungus <br>";
+        } else {
+            echo "KLAIDA: nepavyko prisijungti <br>";
+        }
+    }
+    public function setName($n) {
+        $this->username = $n;
+    }
+    public function getName() {
+        return $this->username;
+    }
+}
+$erelis = new User();
+$erelis->checkLogin("Tim", "Dragon");
+// 2
+class Admin extends User {
+    public $userRights = '';
+    public function changeUsername($newName) {
+        $this->username = $newName;
+    }
+    // automatiskai iskvieciama f-ja
+    function __construct($x = "") {
+        $this->userRights = $x;
+        echo " Ka tik ponai sukurete OBJ <br>";
+    }
+}
+
+$moderatorius = new Admin("moderator");
+$administratorius = new Admin( );
+echo "vartotojo teises: $moderatorius->userRights";
+echo "admino username: $administratorius->"
+
+
+
+//
