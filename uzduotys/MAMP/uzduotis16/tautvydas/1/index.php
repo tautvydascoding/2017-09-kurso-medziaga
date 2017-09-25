d107e161b7654eae91854eb9da844a1a9fd3029d
tautvydascoding <tautvydascoding@gmail.com>
2017-09-25 14:13:06 +0300

    aaa

--- uzduotys/MAMP/uzduotis16/tautvydas/1/index.php
+++ uzduotys/MAMP/uzduotis16/tautvydas/1/index.php
@@ -37,15 +37,64 @@
        }
    }
 
+      // $vartotojas = getUser(1);
+      // var_dump($vartotojas);
+      // print_r ($vartotojas);
+      // echo "Vartotojo id: " . $vartotojas['id'] . "<br />";
+      // echo "Vartotojo vardas: " . $vartotojas['username'] . "<br />";
+      // echo "Vartotojo slaptazodis: " . $vartotojas['pass'] . "<br />";
+      // echo "Vartotojo el. pastas: " . $vartotojas['email'] . "<br />";
+      // echo "Vartotojo el. teises: " . $vartotojas['rights'] . "<br />";
 
-   $vartotojas = getUser(1);
-   // var_dump($vartotojas);
-   // print_r ($vartotojas);
+
+   // paimti konkretu vartotoja pagal jo id
+   function getNewestUser() {
+       $sql = "SELECT * FROM users
+              ORDER BY id DESC
+              LIMIT 1";
+       $rezultatai = mysqli_query(getConnection(), $sql);
+       if ($rezultatai) {
+           //    echo "getUser Suveike";
+           // gryzusisu duomenis is DB sudedame i masyva
+           $rezultatai = mysqli_fetch_assoc($rezultatai);
+           return $rezultatai;
+       } else {
+           echo "ERROR: getUser nesuveike!!!!!!" . mysqli_error(getConnection());
+           return null;
+       }
+   }
+
+
+   $vartotojas = getNewestUser();
    echo "Vartotojo id: " . $vartotojas['id'] . "<br />";
    echo "Vartotojo vardas: " . $vartotojas['username'] . "<br />";
    echo "Vartotojo slaptazodis: " . $vartotojas['pass'] . "<br />";
    echo "Vartotojo el. pastas: " . $vartotojas['email'] . "<br />";
    echo "Vartotojo el. teises: " . $vartotojas['rights'] . "<br />";
+
+
+
+
+   // uzduotis 2
+   // sukurti f-ja createUser($uname, $password, $elpastas, $teises)
+   function createUser($uname, $password, $elpastas, $teises) {
+       $sql_string = "INSERT INTO users
+                      VALUES ('', '$uname', '$password', '$elpastas', '$teises')";
+        $arVeikia = mysqli_query(getConnection(), $sql_string);
+        if (!$arVeikia) {  //  ($arVeikia == false)
+            echo "ERROR: " . mysqli_error(getConnection());
+        }
+   }
+   createUser('Petras', 'xxd3f', 'ada@a.lt', 'subscriber');
+   $vartotojas = getUser(3);
+   echo "Vartotojo id: " . $vartotojas['id'] . "<br />";
+   echo "Vartotojo vardas: " . $vartotojas['username'] . "<br />";
+   echo "Vartotojo slaptazodis: " . $vartotojas['pass'] . "<br />";
+   echo "Vartotojo el. pastas: " . $vartotojas['email'] . "<br />";
+   echo "Vartotojo el. teises: " . $vartotojas['rights'] . "<br />";
+
+
+
 
  // uzduotis 2
  // sukurti f-ja createUser($uname, $password, $elpastas, $teises)
@@ -57,8 +106,17 @@
  // sukurti f-ja getUsers();
 
 
+// $phash = password_hash($pword, PASSWORD_DEFAULT);  // 255 chars
+// $phash = md5($pword);
+ // if ($arPavykoSQL == false) {
+ //   echo "ERROR: xxxx() -  nepavyko: " . mysqli_error($connect) . " <br>";
+ // }
 
 
+
+
+
+// if( mysqli_num_rows($result) > 0 ) {
 
 
 
