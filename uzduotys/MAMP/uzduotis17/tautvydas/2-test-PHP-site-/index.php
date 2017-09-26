b4ae51fe36e99edf479cef5625177305ca3d90a3
tautvydascoding <tautvydascoding@gmail.com>
2017-09-26 15:54:55 +0300

    aaa

--- uzduotys/MAMP/uzduotis17/tautvydas/2-test-PHP-site-/index.php
+++ uzduotys/MAMP/uzduotis17/tautvydas/2-test-PHP-site-/index.php
@@ -1,26 +1,7 @@
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <title></title>
-
-
-        <link rel="stylesheet" href="./lib/bootstrap/css/bootstrap.css">
-        <!-- normalize - sutvarko kai kuriuos browser'io CSS nustaytmus (default) -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/normalize/7.0.0/normalize.min.css">
-        <!-- reikalintas taisyklingam responsice atvaizdavimui -->
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <!-- savo CSS visada zemiau -->
-        <link rel="stylesheet" href="./css/main.css">
-    </head>
-    <body>
-        <div class="container">
-                <header class="row aukstis100 fonas-yellow">
-                    <div class="col-md-12">
-                        header
-                    </div> <!-- END  -->
-                </header>
-
+<?php
+    require_once('./header.php');
+ ?>
+<!-- ============content=============== -->
 
                 <div id="wrapper" class="row   aukstis300">
                      <section class="col-md-8 ">
@@ -32,12 +13,7 @@
                 </div>
                 <!-- END  wrapper-->
 
-                <footer class="row aukstis100 fonas-yellow">
-                    <div class="col-md-12">
-                        Footer
-                    </div>
-                </footer>
-        </div>
-        <!-- END container -->
-    </body>
-</html>
+<!-- ============FOOTER=============== -->
+<?php
+    require_once('./footer.php');
+ ?>
