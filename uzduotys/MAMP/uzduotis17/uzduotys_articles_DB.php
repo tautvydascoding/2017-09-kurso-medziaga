10ecf35370b45b37893ad679ac30f5b2a233f1ec
tautvydascoding <tautvydascoding@gmail.com>
2017-09-26 11:17:25 +0300

    aaa

---
+++ uzduotys/MAMP/uzduotis17/uzduotys_articles_DB.php
@@ -0,0 +1,17 @@
+<?php  
+// uzduotis 1.1
+// sukurti f-ja getArticle($id);
+
+// uzduotis 1.2
+ // grazina straipnsni pagal autoriaus ID
+// sukurti f-ja getArticleByUserId($id);
+
+ // uzduotis 2
+ // sukurti f-ja createArticle( )
+
+ // uzduotis 3
+ // sukurti f-ja deleteArticle( )
+ // uzduotis 4
+ // sukurti f-ja editeArticle(  );
+ // uzduotis 5
+ // sukurti f-ja getArticles();
