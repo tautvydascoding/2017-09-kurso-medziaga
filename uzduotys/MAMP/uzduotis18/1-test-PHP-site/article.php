c31211b0bdfc1f8fd3d00583616522706be95c04
tautvydascoding <tautvydascoding@gmail.com>
2017-09-27 10:24:57 +0300

    18

---
+++ uzduotys/MAMP/uzduotis18/1-test-PHP-site/article.php
@@ -0,0 +1,15 @@
+<article class="row">
+    <div class="col-xl-3  col-lg-4 col-md-6 col-sm-12 ">
+        <img class="img-responsive img-fluid" src="./1.jpg" alt="piestukas rasyti ant popieriaus">
+    </div>
+    <div class="col-xl-9 col-lg-8 col-md-6 col-sm-12 ">
+        <h3>Antraste</h3>
+        <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+        </p>
+        <i>Autorius</i>
+        <i class="float-right pull-right">
+            2017-02-12
+        </i>
+    </div>
+</article>
