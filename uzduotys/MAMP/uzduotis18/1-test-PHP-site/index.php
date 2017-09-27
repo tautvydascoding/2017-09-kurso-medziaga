28fe4472fd83360b805cd06b13a68881c069ff90
tautvydascoding <tautvydascoding@gmail.com>
2017-09-27 12:39:54 +0300

    18

--- uzduotys/MAMP/uzduotis18/1-test-PHP-site/index.php
+++ uzduotys/MAMP/uzduotis18/1-test-PHP-site/index.php
@@ -20,7 +20,22 @@
                              ?>
                      </section>
                      <aside class="col-md-4">
-                         Aside
+                         <h3>Registration</h3>
+                          <form class="form" action="./registration.php" method="post">
+                              <label for="uname">Username</label> <br>
+                              <input type="text" name="uname"  required  > <br>
+
+                              <label for="pass">Password</label>   <br>
+                              <input type="password" name="pass"  required > <br>
+
+                              <label for="pass2">Re-Password</label>   <br>
+                              <input type="password" name="pass2"  required" > <br>
+
+                              <label for="email">Email</label>   <br>
+                              <input type="email" name="email"  required > <br>
+
+                              <input class="btn btn-lg btn-info" type="submit" name="" value="Registration">
+                          </form>
                      </aside>
                 </div>
                 <!-- END  wrapper-->
