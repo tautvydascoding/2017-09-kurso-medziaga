e8cd3119b011b9236745055813592d117dc43c3e
tautvydascoding <tautvydascoding@gmail.com>
2017-09-27 11:33:12 +0300

    18

--- uzduotys/MAMP/uzduotis18/1-test-PHP-site/index.php
+++ uzduotys/MAMP/uzduotis18/1-test-PHP-site/index.php
@@ -6,15 +6,17 @@
                 <div id="wrapper" class="row   aukstis300">
                      <section class="col-md-8 ">
                             <?php
-                                $straipsniai = getArticles(6);
+                                // getArticles - musu suprogramuota
+                                // paima straipsnius is DB
+                                $straipsniai = getArticles(2);
+                                // paimti straipnsni
                                 $straipsnis = mysqli_fetch_assoc($straipsniai);
-                                include('./article.php');
-                                $straipsnis = mysqli_fetch_assoc($straipsniai);
-                                include('./article.php');
-                                $straipsnis = mysqli_fetch_assoc($straipsniai);
-                                include('./article.php');
-                                $straipsnis = mysqli_fetch_assoc($straipsniai);
-                                include('./article.php');
+
+                                while ($straipsnis) {
+                                    include('./article.php');
+                                    // imame sekanti straipnsni
+                                    $straipsnis = mysqli_fetch_assoc($straipsniai);
+                                }
                              ?>
                      </section>
                      <aside class="col-md-4">
