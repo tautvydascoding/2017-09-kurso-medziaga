a27d75a9e2c0bf1c8510e9f8cbb8391938ee054e
semukas <windowas@gmail.com>
2017-10-02 14:06:01 +0300

    the things has cgangeed

--- uzduotys/MAMP/PROJEKTAI/mantvydas/FinalProject/1/function-users.php
+++ uzduotys/MAMP/PROJEKTAI/mantvydas/FinalProject/1/function-users.php
@@ -11,7 +11,7 @@
 
    if ($connection) {
    } else {
-       die ("ERROR: prisijungti napavyko, nes: " . mysqli_connect_error());
+       die ("ERROR failed to connect: " . mysqli_connect_error());
    }
 
    function getConnection() {
@@ -22,12 +22,12 @@
    function getUser($id) {
        $sql = "SELECT * FROM users
               WHERE id = $id";
-       $rezultatai = mysqli_query(getConnection(), $sql);
-       if ($rezultatai) {
-           $rezultatai = mysqli_fetch_assoc($rezultatai);
-           return $rezultatai;
+       $stuff = mysqli_query(getConnection(), $sql);
+       if ($stuff) {
+           $stuff = mysqli_fetch_assoc($stuff);
+           return $stuff;
        } else {
-           echo "ERROR: getUser nesuveike!!!!!!";
+           echo "ERROR: getUser failed!";
            return null;
        }
    }
@@ -36,35 +36,35 @@
        $sql = "SELECT * FROM users
               ORDER BY id DESC
               LIMIT 1";
-       $rezultatai = mysqli_query(getConnection(), $sql);
-       if ($rezultatai) {
-           $rezultatai = mysqli_fetch_assoc($rezultatai);
-           return $rezultatai;
+       $stuff = mysqli_query(getConnection(), $sql);
+       if ($stuff) {
+           $stuff = mysqli_fetch_assoc($stuff);
+           return $stuff;
        } else {
-           echo "ERROR: getUser nesuveike!!!!!!" . mysqli_error(getConnection());
+           echo "ERROR: getUser failed!" . mysqli_error(getConnection());
            return null;
        }
    }
 
 
-   function createUser($uname, $password, $elpastas ) {
+   function createUser($userName, $password, $eMail ) {
        $sql_string = sprintf("INSERT INTO users
                           VALUES ('', '%s', '%s', '%s', 'subscriber')",
-                            mysqli_real_escape_string (getConnection(), $uname),
+                            mysqli_real_escape_string (getConnection(), $userName),
                             mysqli_real_escape_string (getConnection(),  password_hash($password, PASSWORD_DEFAULT) ),
-                            mysqli_real_escape_string (getConnection(), $elpastas)
+                            mysqli_real_escape_string (getConnection(), $eMail)
                      );
 
-        $arVeikia = mysqli_query(getConnection(), $sql_string);
-        if (!$arVeikia) {  //  ($arVeikia == false)
+        $success = mysqli_query(getConnection(), $sql_string);
+        if (!$success) {  //  ($success == false)
             echo "ERROR: " . mysqli_error(getConnection());
         }
    }
 
    function deleteUser ($x) {
        $sql_text =  "DELETE FROM users WHERE id = $x";
-       $arPavykoSQL = mysqli_query(getConnection(), $sql_text);
-       if (!$arPavykoSQL) {  //  ($arVeikia == false)
+       $success = mysqli_query(getConnection(), $sql_text);
+       if (!$success) {  //  ($success == false)
            echo "ERROR: " . mysqli_error(getConnection());
        }
    }
@@ -78,19 +78,19 @@
                         email = '$email',
                         rights = '$rights'
                     WHERE id = $id " ;
-       $arPavykoSQL = mysqli_query(getConnection(), $sql_text);
-       if (!$arPavykoSQL) {  //  ($arVeikia == false)
+       $success = mysqli_query(getConnection(), $sql_text);
+       if (!$success) {  //  ($success == false)
            echo "ERROR: " . mysqli_error(getConnection());
        }
    }
 
-   function getUsers($kiekis = 999999) {
+   function getUsers($kiekis = 9999) {
        $sql_text = "SELECT * FROM users
                     ORDER BY id ASC
                     LIMIT $kiekis;";
-       $rezultatai = mysqli_query(getConnection(), $sql_text);
-       if (mysqli_num_rows($rezultatai) > 0 ) {
-            return $rezultatai;
+       $stuff = mysqli_query(getConnection(), $sql_text);
+       if (mysqli_num_rows($stuff) > 0 ) {
+            return $stuff;
        } else {
            echo "ERROR: " . mysqli_error(getConnection());
            return null;
@@ -101,9 +101,9 @@
 function existUserName($x) {
     $sql_text = "SELECT username FROM users
                     WHERE username = '$x'";
-    $arRado = mysqli_query(getConnection(), $sql_text);
+    $ifFound = mysqli_query(getConnection(), $sql_text);
     // jeigu tokio vardo nerado
-    if (mysqli_num_rows($arRado) > 0 ) {
+    if (mysqli_num_rows($ifFound) > 0 ) {
         echo mysqli_error(getConnection());
         return true;
     } else {
